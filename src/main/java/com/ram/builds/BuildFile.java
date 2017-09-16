ed62248c9b9da9ca6068010a6e22ea8efef16f43
ram7211 <ramanjineyulu.kld@gmail.com>
2017-09-16 22:49:57 +0530

    adding bit functionality

--- src/main/java/com/ram/builds/BuildFile.java
+++ src/main/java/com/ram/builds/BuildFile.java
@@ -1,5 +1,32 @@
 package com.ram.builds;
 
-public class BuildFile {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
+public class BuildFile {
+	
+	
+	static{
+		BuildFile buildFile = new BuildFile();
+		buildFile.load(null);
+	}
+	
+	public void load(String[] args) {
+		Properties properties = new Properties();
+		try {
+			InputStream in = this.getClass().getClassLoader()
+                    .getResourceAsStream("build.properties");
+			properties.load(in);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(properties.getProperty("key"));
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
 }
